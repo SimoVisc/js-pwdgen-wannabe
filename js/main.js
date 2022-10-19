631f46a02ed8734efe32cc8479b86ef669335b8f
SimoVisc <simonaviscardivs@gmail.com>
2022-10-19 17:21:41 +0200

    Aggiunge password

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+// 1.chiedo all'utente il suo nome
+const firstName = prompt(Inserisci il tuo nome);
+// 2.chiedo all'utente il suo cognome
+const lastName = prompt(Inserisci il tuo cognome);
+// 3.chiedo all'utente il suo colore preferito
+const favoriteColor = prompt(Inserisci il tuo colore preferito);
+// 4.creo la password dell'utente
+const Password = $(firstName) $(lastName) $(favoriteColor)76;
+// 5.stampo in console la password dell'utente
+console.log(Password);
+// 6.stampo in html la password dell'utente
+document.getElementById(user-password).innerHTML=Password;
