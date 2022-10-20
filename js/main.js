81116dad1e8374c17d6f2e584622e3e6c2adfbbd
SimoVisc <simonaviscardivs@gmail.com>
2022-10-20 09:40:19 +0200

    Aggiunge virgolette all'interno delle parentesi in main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 "use strict";
 // 1.chiedo all'utente il suo nome
-const firstName = prompt(Inserisci il tuo nome);
+const firstName = prompt("Inserisci il tuo nome");
 // 2.chiedo all'utente il suo cognome
-const lastName = prompt(Inserisci il tuo cognome);
+const lastName = prompt("Inserisci il tuo cognome");
 // 3.chiedo all'utente il suo colore preferito
-const favoriteColor = prompt(Inserisci il tuo colore preferito);
+const favoriteColor = prompt("Inserisci il tuo colore preferito");
 // 4.creo la password dell'utente
-const Password = $(firstName) $(lastName) $(favoriteColor)76;
+const Password = $("firstName") $("lastName") $("favoriteColor") 76;
 // 5.stampo in console la password dell'utente
 console.log(Password);
 // 6.stampo in html la password dell'utente
